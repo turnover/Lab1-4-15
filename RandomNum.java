4a06eae60922911093e816dcb1282d1ac10a7990
turnover <counterparts714@gmail.com>
2014-04-15 20:22:18 -0700

    re-re-lab1

--- RandomNum.java
+++ RandomNum.java
@@ -2,9 +2,10 @@
 public class RandomNum 
 {
 	public int computerNum1;
-	public void GetANumber_Between_1_and_10()
+	public int GetANumber_Between_1_and_10()
 	{
 		computerNum1 = 1 + (int) (Math.random()*10);
+		return computerNum1;
 	}
 	
 }
